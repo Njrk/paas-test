1af679bb3dea2e80dca064617fa8fafa0dfdd13c
root <root@201230.fornex.cloud>
2023-11-08 12:38:43 +0000

    Add log

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "os"
 )
@@ -10,6 +11,7 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "text/html; charset=utf-8")
         fmt.Fprint(w, "<h2> Test app3 (go) </h2>")
+	log.Printf("Request from %s: %s %s\n", r.RemoteAddr, r.Method, r.URL)
     })
 
     port := os.Getenv("PORT")
@@ -17,8 +19,9 @@ func main() {
         port = "5000"
     }
 
-    err := http.ListenAndServe(":" + port, nil)
+    log.Printf("Server is listening on :%s\n", port)
+    err := http.ListenAndServe(":"+port, nil)
     if err != nil {
-        fmt.Println(err)
+        log.Fatalf("Error starting the server: %v", err)
     }
 }
